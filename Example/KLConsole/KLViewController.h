293494aa29806a525b7a9aae5fab3c77bf162710
Kimee <wujm002@galanz.com>
2020-01-06 14:17:54 +0800

    Initial commit

---
+++ Example/KLConsole/KLViewController.h
@@ -0,0 +1,13 @@
+//
+//  KLViewController.h
+//  KLConsole
+//
+//  Created by Kalanhall@163.com on 01/06/2020.
+//  Copyright (c) 2020 Kalanhall@163.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface KLViewController : UIViewController
+
+@end
---
+++ Example/KLConsole/KLViewController.m
@@ -0,0 +1,29 @@
+//
+//  KLViewController.m
+//  KLConsole
+//
+//  Created by Kalanhall@163.com on 01/06/2020.
+//  Copyright (c) 2020 Kalanhall@163.com. All rights reserved.
+//
+
+#import "KLViewController.h"
+
+@interface KLViewController ()
+
+@end
+
+@implementation KLViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
